2915b0d52c80a08ac7a76d8b70e20df64d60eecb
Aziz <15785994-ahmedazizabbassi@users.noreply.gitlab.com>
2024-04-30 14:53:10 +0100

    Fix: date could be undefined

--- src/middleware/payloadFromStringToDate.ts
+++ src/middleware/payloadFromStringToDate.ts
@@ -2,9 +2,9 @@ import { NextFunction, Request, Response } from "express";
 
 export default function (req: Request, res: Response, next: NextFunction) {
     try {
-        req.body.dueDate = new Date(req.body.dueDate);
+        req.body.dueDate = req.body.dueDate ? new Date(req.body.dueDate) : undefined;
     } catch (e: any) {
         req.body.dueDate = undefined;
-     }
+    }
     next();
 }

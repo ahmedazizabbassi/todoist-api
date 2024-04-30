fa012b80762f14422f260da6d281798658e4c00a
Aziz <15785994-ahmedazizabbassi@users.noreply.gitlab.com>
2024-04-30 14:41:34 +0100

    Fix: dueDate is not properly converted

--- src/middleware/payloadFromStringToDate.ts
+++ src/middleware/payloadFromStringToDate.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function (req: Request, res: Response, next: NextFunction) {
-    req.body.dueDate = req.body.dueDate ? new Date(req.body.dueDate) : undefined ;
+    try {
+        req.body.dueDate = new Date(req.body.dueDate);
+    } catch (e: any) {
+        req.body.dueDate = undefined;
+     }
     next();
 }
